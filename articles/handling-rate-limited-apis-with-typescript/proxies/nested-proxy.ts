4359934ff5b8ddf085f468534c174662cdd0f478
Casper Kuethe <casperck64@gmail.com>
2024-11-03 15:13:54 +0100

    Add readme

--- articles/handling-rate-limited-apis-with-typescript/proxies/nested-proxy.ts
+++ articles/handling-rate-limited-apis-with-typescript/proxies/nested-proxy.ts
@@ -4,65 +4,86 @@ import { tryConsume } from "../util.ts";
 import { createProxy } from "./proxy.ts";
 
 function createNestedProxy<T extends object>(root: T, bucket: TokenBucket): T {
-	function createProxy<U extends object>(target: U): U {
-		return new Proxy(target, {
-			get(target, prop, receiver) {
-				const reflectedProperty = Reflect.get(target, prop, receiver);
+  const cache: Map<object, object> = new Map();
 
-				// check if the property is an object
-				if (typeof reflectedProperty === 'object' && reflectedProperty !== null) {
-					// if it is an object create a new nested proxy
-					return createProxy(reflectedProperty)
-				}
-				// check if the property is a function
-				else if (typeof reflectedProperty === "function") {
-					// return a new function with takes in all the arguments and returns a promise
-					// that resolves when the token bucket consumes a token
-					return (...args: unknown[]) => tryConsume(bucket, reflectedProperty.bind(target, ...args))
-				} else {
-					// else just return the property
-					return reflectedProperty;
-				}
-			},
-		});
-	}
+  function createProxy<U extends object>(target: U): U {
+    if (cache.has(target)) {
+      return cache.get(target) as U;
+    }
 
-	return createProxy(root)
+    console.log("creating a proxy for", target);
+
+    const proxy = new Proxy(target, {
+      get(target, prop, receiver) {
+        const reflectedProperty = Reflect.get(target, prop, receiver);
+
+        // check if the property is an object
+        if (
+          typeof reflectedProperty === "object" &&
+          reflectedProperty !== null
+        ) {
+          // if it is an object create a new nested proxy
+          return createProxy(reflectedProperty);
+        }
+        // check if the property is a function
+        else if (typeof reflectedProperty === "function") {
+          // return a new function with takes in all the arguments and returns a promise
+          // that resolves when the token bucket consumes a token
+          return (...args: unknown[]) =>
+            tryConsume(bucket, reflectedProperty.bind(target, ...args));
+        } else {
+          // else just return the property
+          return reflectedProperty;
+        }
+      },
+    });
+
+    cache.set(target, proxy);
+    return proxy;
+  }
+
+  return createProxy(root);
 }
 
 async function noNestedProxy() {
-	// create a bucket
-	const bucket = new TokenBucket(5, 1000);
-	bucket.start();
-	// create the proxied api
-	const proxiedApi = createProxy(usersApi, bucket);
+  // create a bucket
+  const bucket = new TokenBucket(5, 1000);
+  bucket.start();
+  // create the proxied api
+  const proxiedApi = createProxy(usersApi, bucket);
 
-	const users = await proxiedApi.users.getUsers();
-	console.log("Users:", users);
+  const users = await proxiedApi.users.getUsers();
+  console.log("Users:", users);
 
-	console.log(`Being processed by the bucket: ${bucket.getBeingProcessed()}`)
-	bucket.stop();
+  console.log(`Being processed by the bucket: ${bucket.getBeingProcessed()}`);
+  bucket.stop();
 }
 
 async function withNestedProxy() {
-	// create a bucket
-	const bucket = new TokenBucket(5, 1000);
-	bucket.start();
-	// create the proxied api
-	const proxiedApi = createNestedProxy(usersApi, bucket);
+  // create a bucket
+  const bucket = new TokenBucket(5, 1000);
+  bucket.start();
+  // create the proxied api
+  const proxiedApi = createNestedProxy(usersApi, bucket);
 
-	const users = await proxiedApi.users.getUsers();
-	console.log("Users:", users);
+  const users = await proxiedApi.users.getUsers();
+  console.log("Users:", users);
 
-	console.log(`Being processed by the bucket: ${bucket.getBeingProcessed()}`)
-	bucket.stop();
+  // adding a few more calls
+  proxiedApi.users.getUsers();
+  proxiedApi.users.getUsers();
+  proxiedApi.users.getUsers();
+  proxiedApi.users.getUsers();
+
+  console.log(`Being processed by the bucket: ${bucket.getBeingProcessed()}`);
+  bucket.stop();
 }
 
 if (import.meta.main) {
-	console.log("Without nested proxy");
-	console.log("=====================");
-	await noNestedProxy();
-	console.log("\nNested proxy");
-	console.log("=====================");
-	await withNestedProxy();
+  console.log("Without nested proxy");
+  console.log("=====================");
+  await noNestedProxy();
+  console.log("\nNested proxy");
+  console.log("=====================");
+  await withNestedProxy();
 }
